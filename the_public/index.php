321388f4d7e1f986bc4b459248bca38665c90a1d
eacevedof <eacevedof@yahoo.es>
2017-08-29 00:21:53 +0200

    blank

--- the_public/index.php
+++ the_public/index.php
@@ -17,11 +17,11 @@
 <body>
 <div id="divMain" class="container-fluid">
     <div class="row">
-        <br/><br/>
+        <br/>
     </div>
     <div class="row">
         <div class="col-lg-12" style="text-align:center">
-            <a rel="nofollow"  class="" href="https://www.facebook.com/elchalan.aruba/"> 
+            <a rel="nofollow" target="_blank" href="https://www.facebook.com/elchalan.aruba/"> 
                 <img src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/17156040_429150724099395_6365602522345009275_n.jpg?oh=5b2f35183c710fac81dd963b390d565c&oe=5A257B32" alt="El Chalán Aruba - Peruvian Restaurant" class="img-responsive center-block" />
             </a>
         </div>
