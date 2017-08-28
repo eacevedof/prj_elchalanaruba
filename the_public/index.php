f35c3971d50c899217caeaf160b36a06bafc0ae4
eacevedof <eacevedof@yahoo.es>
2017-08-28 23:18:35 +0200

    el chalan aruba

---
+++ the_public/index.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>El Chalán Aruba - Peruvian Cuisine</title>
+    <meta name="description" content="El Chalán in Oranjestad. Peruvian Restaurant in Aruba">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="divMain" class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/17156040_429150724099395_6365602522345009275_n.jpg?oh=5b2f35183c710fac81dd963b390d565c&oe=5A257B32">
+        </div>
+    </div>
+    <p class="text-center">
+<!--elem_totop-->
+        <span class="hidden">
+            <a href="#divMain" class="well well-sm">
+                <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+            </a>
+        </span>
+<!--/elem_totop-->        
+    </p>
+</div>
+<!--elem_footer 1.0.0-->
+<footer class="footer">
+    <div class="container">
+        <ul class="list-inline">
+            <li class="list-inline-item">
+                <a rel="nofollow"  class="btn btn-block" href="/"> 
+                    <span class="fa fa-home"></span>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a rel="nofollow"  class="btn btn-block" href="https://www.facebook.com/elchalan.aruba/"> 
+                    <span class="fa fa-facebook-square"></span>
+                </a>
+            </li>            
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
