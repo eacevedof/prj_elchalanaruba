cabf08e2a6671b5481f1c0bdba6a702445722981
eacevedof <eacevedof@yahoo.es>
2017-10-22 21:17:04 +0200

    /elements/ElementNavbar.js 1.0.0

--- the_reactjs/src/components/elements/ElementNavbar.js
+++ the_reactjs/src/components/elements/ElementNavbar.js
@@ -1,40 +1,31 @@
-//<prj>/the_reactjs/src/components/ElementNavbar.js
+//<prj>/the_reactjs/src/components/elements/ElementNavbar.js 1.0.0
 import React from "react"
 import { connect } from "react-redux"
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const ElementNavbar = (props) => {
-    return (   
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="#">Navbar</a>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/contact/">Contact</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Dishes</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown link
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    return (  
+        <div className="row">
+            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand" href="#">El Chalán - Aruba</a>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact/">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/dishes/">Dishes</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )//return jsx
 }//ElementNavbar
 
