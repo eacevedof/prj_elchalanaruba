8c2f3849df2f4bd3ed31159e2183bacd768db6ba
eacevedof <eacevedof@yahoo.es>
2017-10-23 11:41:28 +0200

    component_sqlite.php v1.0.2 prueba ok sqlite

--- the_application/components/component_sqlite.php
+++ the_application/components/component_sqlite.php
@@ -3,7 +3,7 @@
  * @author Eduardo Acevedo Farje.
  * @link www.eduardoaf.com
  * @name TheApplication\Components\ComponentSqlite 
- * @file component_sqlite.php v1.0.1
+ * @file component_sqlite.php v1.0.2
  * @date 19-09-2017 04:56 SPAIN
  * @observations
  */
@@ -14,13 +14,14 @@ class ComponentSqlite
 
     public function query($sSQL)
     {
+        
         $sPath = TFW_PATH_APPLICATIONDS."db";
-        $dir = "sqlite:/$sPath]/app.db";
-        $dbh  = new PDO($dir) or die("cannot open the database");
+        $dir = "sqlite:$sPath/app.db";
+        $dbh  = new \PDO($dir) or die("cannot open the database");
         $query =  "select * from _template";
         foreach ($dbh->query($query) as $row)
         {
-            echo $row[0];
+            print_r($row);
         }        
     }
     
