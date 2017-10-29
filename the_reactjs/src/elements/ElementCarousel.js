0655433e72fa2729c8298a491e3cb87d8c9568f2
eacevedof <eacevedof@yahoo.es>
2017-10-29 18:43:10 +0100

    <prj>/the_reactjs/src/views/elements/ElementCarousel.js 1.0.0 con errores

---
+++ the_reactjs/src/elements/ElementCarousel.js
@@ -0,0 +1,40 @@
+//<prj>/the_reactjs/src/views/elements/ElementCarousel.js 1.0.0
+import React from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+
+//<div className="jumbotron" style="padding:10px;background: none; border:1px solid #ccc; border-radius: 10px;text-align: center;">
+const ElementCarousel = (props) => {
+    return (  
+        <div className="jumbotron">
+            <h1>
+                <span className="badge badge-default">El Chalán Aruba</span>
+            </h1>
+            <h2>
+                The Best Peruvian Cuisine
+            </h2>
+            <div id="carouselExampleControls" className="carousel slide col-lg-12" data-ride="carousel">
+                <div className="carousel-inner" role="listbox">
+                    <div className="carousel-item active">
+                        <img className="mx-auto d-block img-fluid" src="https://scontent.fmad3-8.fna.fbcdn.net/v/t1.0-9/17156040_429150724099395_6365602522345009275_n.jpg?oh=9c1999b8ad896a645dc3a0befed4e12a&oe=5A749532" alt="First slide">
+                    </div>
+                    <div className="carousel-item">
+                        <img className="mx-auto d-block img-fluid" src="https://scontent.fmad3-8.fna.fbcdn.net/v/t1.0-9/13177628_270159969998472_2075784449577850807_n.jpg?oh=4449072c4bc9978c2d367252fea4158d&oe=5AA89E68" alt="Second slide">
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    )//return jsx
+}//ElementCarousel
+
+export default connect()(ElementCarousel);
+    
+    
