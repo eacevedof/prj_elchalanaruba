6d451f14c8e1ddcd80a28eaedff40ebcbb7379f8
eacevedof <eacevedof@yahoo.es>
2017-11-11 16:36:37 +0100

    error: No Metadata Classes to process.

--- the_application/models/cli-config.php
+++ the_application/models/cli-config.php
@@ -1,10 +1,28 @@
 <?php
+// bootstrap.php
+//die(__DIR__);
+require_once "../../the_vendor/autoload.php";
+
+//require_once '../../the_vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+//$sPathDrivers = realpath(__DIR__ . '/../../lib');
+
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-// replace with file to your own project bootstrap
-require_once "bootstrap.php";
+$isDevMode = TRUE;
 
-// replace with mechanism to retrieve EntityManager in your app
-$entityManager = GetEntityManager();
+$sDriverPath = "../../the_vendor/doctrine/annotations/lib/Doctrine/Common/Annotations";
+$sDriverPath = realpath($sDriverPath);
+$config = Setup::createAnnotationMetadataConfiguration([$sDriverPath],$isDevMode);
+
+$conn = array(
+    "driver" => "pdo_sqlite",
+    "path" => "C:/xampp/htdocs/prj_elchalanaruba/the_application/appdb/db_elchalan.sqlite3",
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);  
 
 return ConsoleRunner::createHelperSet($entityManager);
